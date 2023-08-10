9eb9abf164e04c7f79074228ec4a3d65c7f31e28
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-08-10 16:49:24 +0200

    Add files via upload

---
+++ H1 Fødselsdagen/H1 Fødselsdagen/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Fødselsdagen
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Gives the user informanten for the syntax of the date 
+            Console.WriteLine("Du kan skifte mønsteret ud til f.eks. dd-MM-yyyy");
+            Console.WriteLine("Det indlæste fra brugeren skal følge mønsteret 01062008");
+            
+            //Convert the string to a DateTime
+            DateTime birthday = DateTime.ParseExact(Console.ReadLine(), "ddMMyyyy", null);
+
+            //Gets the difference in days
+            DateTime now = DateTime.Now;
+            TimeSpan difference = now - birthday;
+
+            //uses a calculating to get the years in integer
+            int years = (int)Math.Floor(difference.TotalDays / 365.2425);
+
+
+            //Show the result to the user
+            Console.Clear();
+            Console.WriteLine(years + " År");
+
+            //Stops the program
+            Console.ReadLine();
+        }
+    }
+}
