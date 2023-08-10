10074363effa4a6c484b00d1ab0ddcb19401417a
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-08-10 13:21:28 +0200

    Add files via upload

---
+++ H1 Temperaturberegneren/H1 Temperaturberegneren/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Temperaturberegneren
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int Celsius = 0;
+            
+
+            //A control to see if the number the user has put into the console is valid
+            try
+            {
+                //Gets the users number from the console
+                Celsius = Convert.ToInt32(Console.ReadLine());
+            }
+            //Informain and reset the program
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Skriv et nummer");
+                Console.ReadLine();
+                Console.Clear();
+                Main();
+            }
+                
+            
+
+            //calculation for Fahrenheit and Reaumu from Celsius
+            float Fahrenheit = (Celsius * 1.8f) + 32;
+            float Reaumu = Celsius * 0.8f;
+
+            //Write the calculations to the console
+            Console.WriteLine(Fahrenheit + " Fahrenheit");
+            Console.WriteLine(Reaumu + " Reaumu");
+
+
+            //Stops the program from stopping 
+            Console.ReadLine();
+
+            //Resets program
+            Console.Clear();
+            Main();
+
+        }
+    }
+}
