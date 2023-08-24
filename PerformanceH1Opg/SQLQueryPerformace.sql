3d1a47a4a895a4072d60eb9ecfad5756cd4d3e26
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-08-24 13:25:56 +0200

    Add files via upload

---
+++ PerformanceH1Opg/SQLQueryPerformace.sql
@@ -0,0 +1,24 @@
+
+--Opg Create index
+/* 
+
+CREATE INDEX idx_random
+ON Random(RandomNumber);
+*/
+--Opgave 1
+/*
+SELECT CURRENT_TIMESTAMP
+SELECT * from Random;
+SELECT CURRENT_TIMESTAMP
+*/
+--Opgave 2
+/*
+SELECT Count(RandomNumber) as r,RandomNumber FROM Random
+group by RandomNumber
+ORDER BY r DESC 
+
+SELECT CURRENT_TIMESTAMP
+*/
+--opgave 3,4,5 
+/*it's in c# */
+
