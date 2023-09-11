742dbbb03d2c86e36e123a3b4cb4ae5fe694e778
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-09-11 10:44:09 +0200

    Add files via upload

---
+++ H1 Superheltene/H1 Superheltene/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Superheltene
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Superhero superhero = new Superhero();  
+        }
+    }
+}
---
+++ H1 Superheltene/H1 Superheltene/Superhero.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Superheltene
+{
+    /// <summary>
+    /// class <c>Superhero</c> models a Superhero's characteristics
+    /// </summary>
+    public class Superhero
+    {
+    #region Properties
+        private string _superheroName = "";
+        private string _name = "";
+        private int _age = -1;
+        private string[] _abilities = { "" };
+        private string[] _moralCode = { "" };
+        private string[] _archenemys = { "" };
+        private bool _mask = true;
+    #endregion
+
+    #region public methods
+        public string SuperheroName 
+        { 
+            get { return _superheroName; }
+            private set { _superheroName = value; } 
+        }
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public string[] Abilities
+        {
+            get { return _abilities; }
+            private set { _abilities = value; }
+        }
+        public string[] MoralCode
+        {
+            get { return _moralCode; }
+            private set { _moralCode = value; }
+        }
+        public string[] Archenemys
+        {
+            get { return _archenemys; }
+            private set { _archenemys = value; }
+        }
+        public bool Mask
+        {
+            get { return _mask; }
+            private set { _mask = value; }
+        }
+    #endregion
+    }
+}
