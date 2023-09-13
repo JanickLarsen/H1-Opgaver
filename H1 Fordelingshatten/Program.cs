a14e138f59b97247fdaa34e84e333be9d484d1ad
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-09-13 09:05:42 +0200

    Add files via upload

---
+++ H1 Fordelingshatten/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Fordelingshatten
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SortingHat sortingHat = new SortingHat();
+            while (true) {               
+                Console.WriteLine(sortingHat.GetCollege());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ H1 Fordelingshatten/SortingHat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Fordelingshatten
+{   
+    public class SortingHat
+    {
+        enum Colleges { Gryffindor, Hufflepuff, Ravenclaw, Slytherin };
+        public string GetCollege()
+        {
+            Random random = new Random();
+            return ((Colleges)random.Next(0,4)).ToString();
+        }
+
+    }
+}
