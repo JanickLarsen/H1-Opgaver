195f9d2f97bcbad19c95ec8975b8986785789104
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-09-15 12:29:48 +0200

    Add files via upload

---
+++ H1 Jedi mind tricks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Jedi_mind_tricks
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> people = new Dictionary<string, int>()
+            {
+                { "Luke", 1 },
+                { "Han", 2 },
+                { "Chewbacca", 3 },
+            };
+            people.Remove("Han");
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
